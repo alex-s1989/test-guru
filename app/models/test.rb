9021ce097b7e09209b459de5d2f7fd5a864ae881
alex <shu5388369@gmail.com>
2019-04-20 00:36:32 +0300

    part9_commit1

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
-  has_many :users_tests
+  has_many :test_passages
+  has_many :users, through: :test_passages
   has_many :questions, dependent: :destroy
-  has_many :users, through: :users_tests
   belongs_to :category
   belongs_to :test_author, class_name: 'User', foreign_key: 'author_id'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :users_tests
-  has_many :tests, through: :users_tests
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :test_author, class_name: 'Test', foreign_key: 'author_id'
 
   validates :email, presence: true
@@ -9,4 +9,7 @@ class User < ApplicationRecord
     tests.where(level: level)
   end
 
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
