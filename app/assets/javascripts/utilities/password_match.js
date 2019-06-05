a89329736ec0addc7ee8ae7f3b18b0bfac8859f0
alex <shu5388369@gmail.com>
2019-06-05 18:32:13 +0300

    part15_commit1

---
+++ app/assets/javascripts/utilities/password_match.js
@@ -0,0 +1,22 @@
+document.addEventListener('turbolinks:load', function () {
+  var control = document.querySelector('.password-match')
+  if (control) { control.addEventListener('input', validate) }
+})
+
+function validate() {
+
+  var password = document.getElementById('user_password').value
+  var password_con = document.getElementById('user_password_confirmation').value
+
+
+    if (password.length != 0) {
+        if (password == password_con) {
+            document.querySelector('.octicon-thumbsdown').classList.add('hide');
+            document.querySelector('.octicon-thumbsup').classList.remove('hide');
+        } else {
+            document.querySelector('.octicon-thumbsdown').classList.remove('hide');
+            document.querySelector('.octicon-thumbsup').classList.add('hide');
+        }
+    }
+
+}
