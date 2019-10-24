6e8c8aba31911fcba3ba9e8da42194d448dbd866
alex <shu5388369@gmail.com>
2019-10-24 18:47:39 +0300

    part17_commit1

--- db/migrate/20190326181320_add_author_ref_to_tests.rb
+++ db/migrate/20190326181320_add_author_ref_to_tests.rb
@@ -1,7 +1,7 @@
 class AddAuthorRefToTests < ActiveRecord::Migration[5.2]
   def change
     add_reference :tests, :author, foreign_key: { to_table: :users }
-    remove_column :tests, :user_id, :integer
-    remove_column :tests, :test_author_id, :integer
+    # remove_column :tests, :user_id, :integer
+    # remove_column :tests, :test_author_id, :integer
   end
 end
