97c79a416d48f261160030fcea7143c55c2e8ecc
alex <shu5388369@gmail.com>
2019-10-28 21:26:20 +0300

    part17_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'test-guru-shu.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    adsress:              'smtp.gmail.com',
+    address:              'smtp.gmail.com',
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
