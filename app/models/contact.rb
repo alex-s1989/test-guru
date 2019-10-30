9dddb61cad8ef31c64b427fc9be76d20ab841f1b
alex <shu5388369@gmail.com>
2019-10-30 23:38:04 +0300

    part17_contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,8 +7,8 @@ class Contact < MailForm::Base
   def headers
     {
       subject: 'Contact Form',
-      to:      'shu5388369@gmail.com',
-      # to:      "#{ Admin.pluck(:email) }",
+      # to:      'shu5388369@gmail.com',
+      to:      "#{ Admin.pluck(:email) }",
       from:    %("#{name}" <#{email}>)
     }
   end
